a7cc5940695ae5bd688c082ae82823818b0b0940
aki <142689109+hyppyboy@users.noreply.github.com>
2024-11-22 00:56:55 +0900

    [FEAT] stack_find_last() used in rotate operation

--- src/stack_utils.c
+++ src/stack_utils.c
@@ -6,7 +6,7 @@
 /*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 15:21:12 by akyoshid          #+#    #+#             */
-/*   Updated: 2024/11/21 15:21:46 by akyoshid         ###   ########.fr       */
+/*   Updated: 2024/11/21 19:39:11 by akyoshid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,15 @@ int	stack_len(t_node *stack)
 		stack = stack->next;
 	}
 	return (i);
+}
+
+t_node	*stack_find_last(t_node *stack)
+{
+	if (stack == NULL)
+		at_error();
+	while (stack->next != NULL)
+		stack = stack->next;
+	return (stack);
 }
 
 int	stack_check_sorted(t_node *stack)
